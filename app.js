8a3c82fa8e2525ab9745b46600a650c68c0fab9f
Ankit Kumar Maurya <102985018+ankitmaurya2233@users.noreply.github.com>
2023-08-29 12:56:44 +0530

    Add files via upload
    In this we are using body parse

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const http = require('http');
 const express=require('express');
+const bodyparser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("this is first middleware");
-    next();
-})
+app.use(bodyparser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("this is second middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Name"><input type="text" name="size" placeholder="size"><button type="submit">Add product</button></form>');
+});
+
+app.use('/product',(req,res,next)=>{
+        console.log(req.body);
+        res.redirect('/');
+});
+
+
+app.use('/',(req,res,next)=>{
     console.log("this is second middleware");
     res.send('<h1> hello to node js </h1>');
-})
+});
 
 
 app.listen(3000);
